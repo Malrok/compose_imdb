3700dc80666427abb69f48517720ad1a6037a876
malrok <themalrok@gmail.com>
2020-02-17 20:56:24 +0100

    added a navigate back action

--- app/src/main/java/com/mrk/composeimdb/ui/detail/Detail.kt
+++ app/src/main/java/com/mrk/composeimdb/ui/detail/Detail.kt
@@ -6,8 +6,12 @@ import androidx.ui.core.Text
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.layout.Column
 import androidx.ui.material.TopAppBar
+import com.mrk.composeimdb.R
 import com.mrk.composeimdb.repositories.network.TmdbService
+import com.mrk.composeimdb.ui.Screen
+import com.mrk.composeimdb.ui.VectorImageButton
 import com.mrk.composeimdb.ui.common.observe
+import com.mrk.composeimdb.ui.navigateTo
 
 @Composable
 fun MovieDetail(tmdb: TmdbService, movieId: String) {
@@ -18,7 +22,12 @@ fun MovieDetail(tmdb: TmdbService, movieId: String) {
 
         Column {
             TopAppBar(
-                title = { Text(text = "Imdb") }
+                title = { Text(text = "Imdb") },
+                        navigationIcon = {
+                    VectorImageButton(R.drawable.ic_back) {
+                        navigateTo(Screen.MoviesList)
+                    }
+                }
             )
             VerticalScroller(modifier = Flexible(1f)) {
                 Column {
